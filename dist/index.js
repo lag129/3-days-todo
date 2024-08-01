4cad0ce6d9fca48eea17b5aebe5b84ace0387122
Aoki-Dai <108746684+Aoki-Dai@users.noreply.github.com>
2024-08-01 19:34:01 +0900

    feat : チェックボックスを追加

--- dist/index.js
+++ dist/index.js
@@ -46,7 +46,21 @@ formE.addEventListener("submit", (ev) => {
         console.log(`${k}: ${v}`);
 
         const td = document.createElement("td");
-        td.textContent = v;
+
+        if (k === "completed") {
+            // 完了チェックボックスを追加
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox";// チェックボックスのタイプを設定
+            checkbox.checked = v;// 初期状態を設定
+            checkbox.addEventListener("change", (event) => {
+                // チェックボックスの状態が変更されたときに完了状態を更新
+                newTodo.completed = event.target.checked;
+            });
+            td.appendChild(checkbox); // チェックボックスをセルに追加
+        } else {
+            td.textContent = v;// その他のプロパティはテキストとしてセルに追加
+        }
+
         td.classList.add(k);
         tr.appendChild(td);
     }
